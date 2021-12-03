63e9871e193f4ca42d0c3670c8064f5b7e30a332
kakin <kaisboulakhlas9@gmail.com>
2021-12-03 17:33:02 +0100

    test:Mise en place de l'environnement test + test si UserIsLoginSuccessFull

--- src/Security/WebAppAuthenticator.php
+++ src/Security/WebAppAuthenticator.php
@@ -54,8 +54,7 @@ class WebAppAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-        //return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('dashboard'));
     }
 
     protected function getLoginUrl(Request $request): string
--- tests/UserTest.php
+++ tests/UserTest.php
@@ -2,16 +2,34 @@
 
 namespace App\Tests;
 
+
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\RouterInterface;
 
 class UserTest extends WebTestCase
 {
-    public function testSomething(): void
+    public function testIfLoginIsSuccessful(): void
     {
         $client = static::createClient();
-        $crawler = $client->request('GET', '/');
 
-        $this->assertResponseIsSuccessful();
-        $this->assertSelectorTextContains('h1', 'Hello World');
+        /** @var RouterInterface $router */
+        $router = $client->getContainer()->get("router");
+
+        $crawler = $client->request(Request::METHOD_GET, $router->generate("app_login"));
+
+        $form = $crawler->filter("form[name=login]")->form([
+            "email" => "admin@medicalware.fr",
+            "password" => "admin"
+        ]);
+
+        $client->submit($form);
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_FOUND);
+
+        $client->followRedirect();
+
+        $this->assertRouteSame('dashboard');
     }
 }
