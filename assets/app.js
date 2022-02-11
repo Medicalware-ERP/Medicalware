3dcc8e8b926eb17903f65c9f0949eaac13664804
wassayd <saidwaskar@gmail.com>
2022-02-11 16:18:06 +0100

    Style: add sass and refactor styles

--- assets/app.js
+++ assets/app.js
@@ -7,7 +7,7 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import 'normalize.css'
-import './styles/app.css';
+import './styles/app.scss';
 import '@fortawesome/fontawesome-free/css/all.css';
 
 
