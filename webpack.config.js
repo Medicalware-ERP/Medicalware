a03e6d86450ec5970ab4613b8a904740645b3dbc
kakin <kaisboulakhlas9@gmail.com>
2022-03-02 08:52:27 +0100

    Styles: login page + create base template only login page

--- webpack.config.js
+++ webpack.config.js
@@ -72,6 +72,19 @@ Encore
         //pattern: /\.(png|jpg|jpeg)$/
     })
 
+    .copyFiles({
+        from: './assets/videos',
+
+        // optional target path, relative to the output dir
+        to: 'videos/[path][name].[ext]',
+
+        // if versioning is enabled, add the file hash too
+        //to: 'images/[path][name].[hash:8].[ext]',
+
+        // only copy files matching this pattern
+        //pattern: /\.(png|jpg|jpeg)$/
+    })
+
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
 
