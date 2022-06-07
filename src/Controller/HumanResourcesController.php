109104462bb5552c09b64a5c30e1c4296796ec8f
wassayd <saidwaskar@gmail.com>
2022-06-07 10:38:55 +0200

    dashboard add redirect to hr list

--- src/Controller/HumanResourcesController.php
+++ src/Controller/HumanResourcesController.php
@@ -18,7 +18,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class HumanResourcesController extends BaseController
 {
 
-    public function __construct(private EntityManagerInterface $manager)
+    public function __construct(private readonly EntityManagerInterface $manager)
     {
     }
 
