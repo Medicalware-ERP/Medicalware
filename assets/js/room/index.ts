7b25faad0342d3e69ba31cc0f00fc6e0460bde19
Loris Quetglas <quetglas.loris@gmail.com>
2022-06-13 15:45:14 +0200

    Feature : Delete room

--- assets/js/room/index.ts
+++ assets/js/room/index.ts
@@ -1,7 +1,36 @@
 import generateDatable from "../datatable/datatableGeneric";
 import {$} from "../utils";
+import Routing from "../Routing";
+import {swaleWarning} from "../util/swal";
 
 const table = $("#table-rooms");
+
+const deleteRoom = () => {
+
+    const callback = (e: Event) => {
+        e.stopPropagation();
+
+        const button = <HTMLInputElement>e.currentTarget;
+        const text: string = "Vous êtes sur le point de supprimer une salle."
+        const url = Routing.generate('app_delete_room', {
+            id: button.dataset.room
+        });
+
+        swaleWarning(text).then(r => {
+            fetch(url).then(() => {
+                if (table instanceof HTMLTableElement) {
+                    generateDatable(table);
+                }
+            });
+        });
+    };
+
+    $(".btn-delete-room", (elem: Node) => {
+        elem.addEventListener('click', callback)
+    });
+}
+
+document.addEventListener('datatable.loaded', deleteRoom);
 
 if (table instanceof HTMLTableElement) {
     generateDatable(table);
--- src/Controller/RoomController.php
+++ src/Controller/RoomController.php
@@ -3,12 +3,14 @@
 namespace App\Controller;
 
 use App\Entity\Room;
+use App\Repository\RoomRepository;
 use App\Service\Room\RoomDataFormatter;
 use Psr\Container\ContainerExceptionInterface;
 use Psr\Container\NotFoundExceptionInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RoomController extends BaseController
@@ -32,5 +34,18 @@ class RoomController extends BaseController
     public function paginate(Request $request, RoomDataFormatter $roomDataFormatter): JsonResponse
     {
         return $this->paginateRequest(Room::class, $request, $roomDataFormatter);
+    }
+
+    #[Route('/room/delete/{id}', name: 'app_delete_room')]
+    public function delete(int $id, RoomRepository $roomRepository)
+    {
+        $room = $roomRepository->find($id);
+
+        if ($room == null)
+            throw new NotFoundHttpException();
+
+        $roomRepository->remove($room);
+
+        return $this->redirectToRoute("app_room");
     }
 }
