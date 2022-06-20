6b8a3fc8be90d6cfe913187a58e48cb3d1cb4291
Loris Quetglas <quetglas.loris@gmail.com>
2022-06-20 09:56:20 +0200

    Feature : EnumController => add can now persist data

--- src/Controller/EnumController.php
+++ src/Controller/EnumController.php
@@ -8,19 +8,27 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 class EnumController extends AbstractController
 {
+    public function __construct(private readonly EntityManagerInterface $manager)
+    {
+    }
 
     #[Route('/addEnum/{class}', name: 'add_enum')]
     public function add(Request $request, string $class): Response
     {
         $data = new $class();
-        $form = $this->createForm(EnumType::class, $data);
+        $form = $this->createForm(EnumType::class, $data, [ "action" => $request->getUri() ]);
         $form->handleRequest($request);
 
         if($form->isSubmitted() && $form->isValid()){
-            dd($data);
+            $this->manager->persist($data);
+            $this->manager->flush();
+
+            $referer = $request->headers->get('referer');
+            return $this->redirect($referer);
         }
 
         return $this->renderForm("enum/_form.html.twig", [
