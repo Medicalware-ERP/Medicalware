cef409178b5c3d8344bc8c5ca93da3885190f965
Loris Quetglas <quetglas.loris@gmail.com>
2022-06-20 10:57:34 +0200

    Feature : Add edit method in EnumController

--- src/Controller/EnumController.php
+++ src/Controller/EnumController.php
@@ -35,4 +35,25 @@ class EnumController extends AbstractController
             "form" => $form
         ]);
     }
+
+    #[Route('/editEnum/{class}/{id}', name: 'edit_enum')]
+    public function edit(Request $request, string $class, int $id): Response
+    {
+        $data = $this->manager->find($class, $id) ?? throw new NotFoundHttpException("Entité non trouvée");
+        $form = $this->createForm(EnumType::class, $data, [ "action" => $request->getUri() ]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->manager->persist($data);
+            $this->manager->flush();
+
+            $referer = $request->headers->get('referer');
+            return $this->redirect($referer);
+        }
+
+        return $this->renderForm("enum/_form.html.twig", [
+            "form" => $form,
+            "data" => $data
+        ]);
+    }
 }
