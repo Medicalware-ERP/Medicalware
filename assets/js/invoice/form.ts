6df07d51a0b8ad49743745d0e51e2aab082d2285
wassayd <saidwaskar@gmail.com>
2022-06-22 11:05:15 +0200

    Fix: form collection order and invoice

--- assets/js/invoice/form.ts
+++ assets/js/invoice/form.ts
@@ -1,3 +1,6 @@
+import {initFormCollection} from "../util/form_collection";
+
+initFormCollection();
 
 document.addEventListener('collection.element.added', (e: Event) => {
     console.log((e as CustomEvent).detail.element)
