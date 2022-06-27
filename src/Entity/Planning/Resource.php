215a8dabbaf4e7d3ef2582b97d85131ba2ed6d6d
Loris Quetglas <quetglas.loris@gmail.com>
2022-06-27 10:04:06 +0200

    Feature : Added a Resource class that is used in event

---
+++ src/Entity/Planning/Resource.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Entity\Planning;
+
+use App\Repository\Planning\ResourceRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ResourceRepository::class)]
+class Resource
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'integer')]
+    private $resourceId;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $resourceClass;
+
+    #[ORM\OneToMany(mappedBy: 'resource', targetEntity: Event::class, orphanRemoval: true)]
+    private $events;
+
+    public function __construct()
+    {
+        $this->events = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getResourceId(): ?int
+    {
+        return $this->resourceId;
+    }
+
+    public function setResourceId(int $resourceId): self
+    {
+        $this->resourceId = $resourceId;
+
+        return $this;
+    }
+
+    public function getResourceClass(): ?string
+    {
+        return $this->resourceClass;
+    }
+
+    public function setResourceClass(string $resourceClass): self
+    {
+        $this->resourceClass = $resourceClass;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Event>
+     */
+    public function getEvents(): Collection
+    {
+        return $this->events;
+    }
+
+    public function addEvent(Event $event): self
+    {
+        if (!$this->events->contains($event)) {
+            $this->events[] = $event;
+            $event->setResource($this);
+        }
+
+        return $this;
+    }
+
+    public function removeEvent(Event $event): self
+    {
+        if ($this->events->removeElement($event)) {
+            // set the owning side to null (unless already changed)
+            if ($event->getResource() === $this) {
+                $event->setResource(null);
+            }
+        }
+
+        return $this;
+    }
+}
---
+++ src/Service/Planning/ResourceService.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Service\Planning;
+
+use App\Entity\EntityInterface;
+use App\Entity\Planning\Resource;
+use Doctrine\ORM\EntityManagerInterface;
+
+class ResourceService
+{
+    public function __construct(private readonly EntityManagerInterface $manager)
+    {
+    }
+
+    public function getOrCreateResource(int $id, string $class): Resource
+    {
+        $resource = $this->manager->getRepository(Resource::class)->findOneBy(["resourceClass" => $class, "resourceId" => $id]);
+
+        if ($resource == null) {
+            $resource = new Resource();
+            $resource->setResourceId($id);
+            $resource->setResourceClass($class);
+        }
+
+        return $resource;
+    }
+}
