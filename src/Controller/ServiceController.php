eba9ea41cb786a1176ca8e3c9282f2178ce4a16e
kakin <kaisboulakhlas9@gmail.com>
2022-06-28 15:46:05 +0200

    Fix archivedAt service

--- src/Controller/ServiceController.php
+++ src/Controller/ServiceController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Patient;
 use App\Entity\Provider;
 use App\Entity\Service;
 use App\Form\ProviderType;
@@ -10,18 +11,27 @@ use App\Repository\ProviderRepository;
 use App\Repository\ServiceRepository;
 use App\Service\Provider\ProviderDataFormatter;
 use App\Service\Service\ServiceDataFormatter;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\OptimisticLockException;
 use Doctrine\ORM\ORMException;
+use Doctrine\ORM\QueryBuilder;
 use Psr\Container\ContainerExceptionInterface;
 use Psr\Container\NotFoundExceptionInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[IsGranted("ROLE_ADMIN_SERVICE")]
 class ServiceController extends BaseController
 {
+    public function __construct(private readonly EntityManagerInterface $manager)
+    {
+    }
+
     #[Route('/service', name: 'service_index')]
     public function index(): Response
     {
@@ -38,7 +48,28 @@ class ServiceController extends BaseController
     #[Route('/serviceJson', name: 'service_json')]
     public function paginate(Request $request, ServiceDataFormatter $serviceDataFormatter): JsonResponse
     {
-        return $this->paginateRequest(Service::class, $request, $serviceDataFormatter);
+        $modifier = function(QueryBuilder $queryBuilder){
+            $queryBuilder->andWhere("e.isArchived = 0");
+        };
+
+        return $this->paginateRequest(Service::class, $request, $serviceDataFormatter, $modifier);
+    }
+
+    #[Route('/service/toArchive/{id}', name: 'app_to_archive_service')]
+    public function archive(int $id): Response
+    {
+        $service = $this->manager->find(Service::class, $id) ?? throw new NotFoundHttpException("Patient non trouvé");
+
+        if ($service == null){
+            throw new NotFoundHttpException();
+        }
+
+        $service->setIsArchived(true);
+        $service->setIsArchivedAt(new \DateTimeImmutable());
+
+        $this->manager->persist($service);
+        $this->manager->flush();
+        return $this->redirectToRoute("service_index");
     }
 
     /**
