dea6de58e8adc85a43b7816952779312cd69ceda
wassayd <saidwaskar@gmail.com>
2022-06-29 22:13:01 +0200

    Fix: subject mail reset password

--- src/Controller/ResetPasswordController.php
+++ src/Controller/ResetPasswordController.php
@@ -162,7 +162,7 @@ class ResetPasswordController extends AbstractController
         $email = (new TemplatedEmail())
             ->from(new Address('admin@medicalware.com', 'Medicalware'))
             ->to($user->getEmail())
-            ->subject('Your password reset request')
+            ->subject('Demande de changement du mot de passe')
             ->htmlTemplate('reset_password/email.html.twig')
             ->context([
                 'resetToken' => $resetToken,
